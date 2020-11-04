a8a59384243d15e69a41352fc968fc1d1c113b36
Heba Masoud <heba.masoud@patelhs.org>
2020-11-04 16:50:08 +0000

    added comment

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 class Main {
   public static void main(String[] args) {
    //Create an array of 10 int values (must have both even & odd numbers) 
-   int[] numbers = {};
+   int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+   //Use a For or While loop to loop backwards through array
    
+
   }
 }
