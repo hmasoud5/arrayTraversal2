4af2ef285154e7ff7044b7c438448522df64970e
Heba Masoud <heba.masoud@patelhs.org>
2020-11-04 16:57:42 +0000

    added print statement

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ class Main {
    int[] numbers = {1,2,3,4,5,6,7,8,9,10};
    //Use a For or While loop to loop backwards through array
    for(int i = 9; i >=0;i--){
-     //Only print the value if it is even
      if(numbers[i] %2 == 0 ){
-
+            //Only print the value if it is even
+            System.out.println(numbers[i]);
      }
      
    }
